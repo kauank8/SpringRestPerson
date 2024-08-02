fcb8cafe2f43802b902289f2ffb4ac47ab76a9d6
Kauan Paulino Farias <82879273+kauank8@users.noreply.github.com>
2024-08-02 17:05:42 -0300

    Content Negotiation
    Adicionando o conceito de Content Negotiation, implementando via Query Param e Header Param. Adicionando classes de configuração e MediaType, além de produzir e consumir Json, Xml e Yaml.

--- SpringRestPerson/src/main/java/farias/paulino/kauan/SpringRestPerson/controller/PersonController.java
+++ SpringRestPerson/src/main/java/farias/paulino/kauan/SpringRestPerson/controller/PersonController.java
@@ -3,7 +3,6 @@ package farias.paulino.kauan.SpringRestPerson.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import farias.paulino.kauan.SpringRestPerson.data.vo.v1.PersonVO;
 import farias.paulino.kauan.SpringRestPerson.data.vo.v2.PersonVOV2;
 import farias.paulino.kauan.SpringRestPerson.services.PersonServices;
+import farias.paulino.kauan.SpringRestPerson.util.MediaType;
 
 @RestController
 @RequestMapping("/person")
@@ -24,31 +24,34 @@ public class PersonController {
 	@Autowired
 	PersonServices service;
 
-	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	public PersonVO findById(@PathVariable(value = "id") Long id) throws Exception {
 		return service.findById(id);
 
 	}
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public List<PersonVO> findByAll() throws Exception {
 		return service.findAll();
 
 	}
 
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVO create(@RequestBody PersonVO person) throws Exception {
 		return service.create(person);
 
 	}
 	
-	@PostMapping(value="/v2",produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(value="/v2",produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVOV2 createV2(@RequestBody PersonVOV2 person) throws Exception {
 		return service.createV2(person);
 		
 	}
 
-	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	@PutMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML}, 
+			consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
 	public PersonVO update(@RequestBody PersonVO person) throws Exception {
 		return service.create(person);
 
